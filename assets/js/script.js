f2a92b699a7882d2020e3e86b101727dc453d293
Rowan Parkinson <rowan.parkinson@gmail.com>
2018-01-15 16:43:16 +1100

    Starting to migrate JS files into a plugin structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,5 @@
-(function($) {
-  'use strict';
+import $ from 'jquery';
 
-  var breakpoint = 768;
-  var altbreakpoint = 991;
-  var $w = $(window);
-  var $d = $(document);
-  var $body = $('body');
-  var windowheight = $w.height() - 135;
+import './plugins/menu';
 
-  $.fn.replaceImages = function () {
-
-    $('.thumb-img').each(function (i, e) {
-
-      var $elem = $(e);
-      var src = 0;
-
-      if ($w.innerWidth() <= altbreakpoint) {
-        src = $elem.attr('data-bg-mobile');
-        $elem.attr( 'src', src );
-      } else {
-        src = $elem.attr('data-bg-desktop');
-        $elem.attr( 'src', src );
-      }
-
-    });
-
-  };
-
-  $w.on('load resize orientationchange', function () {
-
-    $body.replaceImages();
-
-    if (windowheight) {
-      $('.slideshow-container, .thumbnail-container, .loader').css({'height': windowheight});
-    }
-
-    if ($w.innerWidth() <= altbreakpoint) {
-      $('.thumbnail-container').show();
-      $('.slideshow-container').hide();
-    } else {
-      if ($('.thumbnail-container').is(':visible')) {
-        $('.thumbnail-container').show();
-        $('.slideshow-container').hide();
-      } else if ($('.slideshow-container').is(':visible')) {
-        $('.thumbnail-container').hide();
-        $('.slideshow-container').show();
-      } else {
-        $('.thumbnail-container').hide();
-        $('.slideshow-container').show();
-      }
-    }
-  });
-
-})(jQuery);
+$('#menu').Menu();
