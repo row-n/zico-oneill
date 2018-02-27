cf5940396d1c6557e668b4cafee169696baa6840
Rowan Parkinson <rowan.parkinson@gmail.com>
2018-02-27 14:53:20 +1100

    Adding version to assets

--- functions.php
+++ functions.php
@@ -202,7 +202,5 @@ add_filter('show_admin_bar', 'remove_admin_bar'); // Remove Admin bar
 add_filter('nav_menu_item_id', '__return_empty_string'); // Remove id from nav menu items
 add_filter('nav_menu_css_class', 'nav_menu_item_class', 10, 4); // Add class to menu items
 add_filter('nav_menu_link_attributes', 'nav_menu_link_atts', 10, 4); // Add class to menu link
-add_filter('style_loader_src', 'sdt_remove_ver_css_js', 9999); // Remove WP Version From Styles
-add_filter('script_loader_src', 'sdt_remove_ver_css_js', 9999); // Remove WP Version From Scripts
 add_filter('use_default_gallery_style', '__return_false'); // Remove Gallery styles
 add_filter('excerpt_length', 'custom_excerpt_length', 9999); // Limit excerpt length
